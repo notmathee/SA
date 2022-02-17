82f011c2cbee2ef148641a1e29c55b5e94258bc2
mathêe <matheusfc2626@gmail.com>
2022-02-16 22:43:25 -0300

    login pronto, logout parcialmente pronto

--- paginaCadastro.js
+++ paginaCadastro.js
@@ -1,18 +1,21 @@
 let listaDeUsuarios = []
+let usuarioLogado = []
 
 let usuario = {
     nome: '',
     email: '',
     senha: '',
     celular: '',
+    login: false,
 }
 
-function adicionarUsuario() {
+let adicionarUsuario = () => {
     let usuario = {
-        nome: document.getElementById('nomeCadastrado').value,
-        email: document.getElementById('emailCadastrado').value,
-        senha: document.getElementById('senhaCadastrado').value,
-        celular: document.getElementById('celularCadastrado').value
+        nome: document.getElementById('nomeCadastro').value,
+        email: document.getElementById('emailCadastro').value,
+        senha: document.getElementById('senhaCadastro').value,
+        celular: document.getElementById('celularCadastro').value,
+        login: false,
     }
 
     listaDeUsuarios = JSON.parse(localStorage.getItem('listaDeUsuarios'))
@@ -24,40 +27,72 @@ function adicionarUsuario() {
         alert('Todos os campos precisam ser preenchidos!')
         return;
     } else {
-        emailInvalido = false
-        listaDeUsuarios.forEach(usuarioJSON => {
-            if (usuarioJSON.email == usuario.email) {
-                emailInvalido = true
+        emailInvalido = true
+        listaDeUsuarios.forEach(usuarioCadastro => {
+            if (usuarioCadastro.email == usuario.email) {
+                emailInvalido = false
+                alert('Este E-mail existe no cadastro.')
             }
-        }); 
-        if(emailInvalido){
-            alert('Este E-mail existe no cadastro.')
-            return;
+        });
+        if (emailInvalido) {
+            listaDeUsuarios.push(usuario)
+            localStorage.setItem('listaDeUsuarios', JSON.stringify(listaDeUsuarios))
+            alert('dados salvos')
         }
     }
-    listaDeUsuarios.push(usuario)
-    localStorage.setItem('listaDeUsuarios', JSON.stringify(listaDeUsuarios))
+}
 
-    alert('dados salvos')
+let logarUsuario = () => {
+    let email = document.getElementById('emailLogin').value
+    let senha = document.getElementById('senhaLogin').value
+    login = false
+
+    listaDeUsuarios = JSON.parse(localStorage.getItem('listaDeUsuarios'))
+    usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'))
+
+    if (usuarioLogado == null) {
+        usuarioLogado = []
+    }
+    if (email == '' || senha == '') {
+        alert('Todos os campos precisam ser preenchidos!')
+        return;
+    }
+
+    loginInvalido = true
+    listaDeUsuarios.forEach(usuarioLogin => {
+        if (usuarioLogin.email == email && usuarioLogin.senha == senha) {
+            loginInvalido = false
+            usuarioLogin.login = true
+            usuarioLogado.splice(0, 1, [usuarioLogin.nome, email, senha, usuarioLogin.celular, usuarioLogin.login])
+            localStorage.setItem('usuarioLogado', JSON.stringify(usuarioLogado))
+            alert('logado')
+        }
+    });
+    if (loginInvalido) {
+        alert('E-mail ou senha inválidos.')
+    }
+}
+
+let logoutUsuario = () => {
+    usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'))
+
+    for (let i = 0; i < usuarioLogado.length; i++) {
+        if ([i] == 'true') {
+            usuarioLogado[i] = false
+        }   
+    }
 }
 
 let excluirUsuario = () => {
     listaDeUsuarios = JSON.parse(localStorage.getItem('listaDeUsuarios'))
-    
+
     if (usuario.nome == '' || usuario.email == '' || usuario.senha == '' || usuario.celular == '') {
         alert('Todos os campos precisam ser preenchidos!')
         return;
     } else {
         for (let i = 0; i < listaDeUsuarios.length; i++) {
-            i
-            
         }
     }
 }
 
-function listarUsuario() {
-    listaDeUsuarios = JSON.parse(localStorage.getItem('listaDeUsuarios'))
-    console.log(listaDeUsuarios)
-
-}
 
